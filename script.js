2a5fed743f1183ab324bbfa2d9f102e847f965a3
Cidrack <thalisoncidrack72@gmail.com>
2024-12-12 17:18:41 -0300

    implementação da função de atualizar pontos ao decorrer da partida

--- script.js
+++ script.js
@@ -4,4 +4,12 @@
 // Função para atualizar tempo da partida.
 // Função para definir configurações.
 // Função para ir atualizando os pontos conforme o decorrer da partida.
+let pombo = document.getElementById("icone-pombo");
+let spanPontos = document.getElementById("pontos");
+let pontos = 0;
+
+pombo.addEventListener("click", () => {
+    pontos += 15;
+    spanPontos.innerText = `Pontos: ${pontos}`;
+});
 // manter as configurações salvas ao sair do jogo(opicional)
