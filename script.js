d902773c70b4a7dfbca5af391e91759f0d8d9404
DiegoAraaujo <diegoaraaujo8@gmail.com>
2024-12-12 15:32:25 -0300

    planejamento das funções do Js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pombo deve esta dentro do cenario.
+// Botao iniciar deve iniciar iniciar a partida.
+// Função para atualizar o ranking.
+// Função para atualizar tempo da partida.
+// Função para definir configurações.
+// Função para ir atualizando os pontos conforme o decorrer da partida.
+// manter as configurações salvas ao sair do jogo(opicional)
