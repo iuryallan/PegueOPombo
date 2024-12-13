4f1419f80579f351e9feff930a1db1e27c7f38d2
Fabrício M <fabmartins272@gmail.com>
2024-12-12 21:35:15 -0300

    Implementação da função perca de pontos e exibição das configurações padrões básicas

--- script.js
+++ script.js
@@ -14,9 +14,15 @@ buttonConfig.addEventListener("click", () => {
 buttonClose.addEventListener("click", () => {
     interfaceConfig.style.display = "none"
 });
+
 // Função para definir configurações.
+let spanTempo = document.getElementById("tempo");
+let tempo = 30;
+spanTempo.innerText = `Tempo: ${tempo}s`;
+
 
 // Função para ir atualizando os pontos conforme o decorrer da partida.
+
 let pombo = document.getElementById("icone-pombo");
 let spanPontos = document.getElementById("pontos");
 let pontos = 0;
@@ -24,5 +30,18 @@ let pontos = 0;
 pombo.addEventListener("click", () => {
     pontos += 15;
     spanPontos.innerText = `Pontos: ${pontos}`;
+    event.stopPropagation();
 });
+
+let cenario = document.getElementById("cenarioId");
+cenario.addEventListener("click", () => {
+    pontos -= 5;
+    if(pontos < 0){
+        pontos=0;
+    }else{
+        spanPontos.innerText = `Pontos: ${pontos}`;
+    }
+     
+});
+
 // manter as configurações salvas ao sair do jogo(opicional)
